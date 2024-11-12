25f8dc2d427609dbfc761c3c3aef58bd1a44fb9a
NooaV-M <nooa.vainio-mattila@metropolia.fi>
2024-11-12 18:28:31 +0200

    JS mod1 tehtävä 9 done

--- JS-mod-01/Teht9/Teht9.js
+++ JS-mod-01/Teht9/Teht9.js
@@ -2,11 +2,18 @@
 
 const num = prompt('Enter whole number:');
 
-let vastaus
+let vastaus = `${num} on alkuluku`
 
 if (num < 2) {
     vastaus = `${num} ei ole alkuluku`
 }
 else {
-    for
+    for (let i = 2; i < num/2; i++) {
+        if (num % i === 0) {
+            vastaus = `${num} ei ole alkuluku`
+            break;
+        }
+    }
 }
+
+document.querySelector("#result").innerHTML = vastaus;
