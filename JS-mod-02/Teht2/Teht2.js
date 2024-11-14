8a1f56fb8906d2afb30c409f149d2304bd2dab65
NooaV-M <nooa.vainio-mattila@metropolia.fi>
2024-11-14 10:00:47 +0200

    jc mod2 tehtävä 2 done

--- JS-mod-02/Teht2/Teht2.js
+++ JS-mod-02/Teht2/Teht2.js
@@ -1,13 +1,21 @@
 'use strict';
 
-let participant_list = ""
+let participant_list = []
+
+let participant_string = ""
 
 let participant_num = parseInt(prompt("Enter number of participants"))
 
 for(let i = 0; i < participant_num; i++){
-    participant_list += `<li>${prompt("Enter participant name")}</li><br>`;
+    participant_list.push(prompt("Enter participant name"));
 }
 
-document.querySelector("#skibidi").innerHTML = participant_list;
+participant_list.sort()
+
+for(let i = 0; i < participant_num; i++){
+    participant_string += `<li>${participant_list[i]}</li><br>`;
+}
+
+document.querySelector("#skibidi").innerHTML = participant_string;
 
 console.log(participant_list);
